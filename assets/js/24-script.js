231f9e7ba7e24422b2700f6bfa4261cc4b6b1c26
Ziarahman Musleh <ziarahmanmusleh@gmail.com>
2023-10-17 04:54:08 +0300

    day05

--- assets/js/24-script.js
+++ assets/js/24-script.js
@@ -2,13 +2,19 @@
 let mode = "light"
 
 const bodyEl =  document.querySelector("body")
+const buttonEl = document.querySelector("button")
+
 
 function setMode(){
     if(mode=="light"){
+        buttonEl.classList.remove("light");
+        buttonEl.classList.add("dark");
         bodyEl.classList.remove("light");
         bodyEl.classList.add("dark");
         mode = "dark";
     }else{
+        buttonEl.classList.remove("dark");
+        buttonEl.classList.add("light");
         bodyEl.classList.remove("dark");
         bodyEl.classList.add("light");
         mode = "light";
